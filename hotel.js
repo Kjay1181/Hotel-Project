31e7e62ff56f167fd0591534ddf0eb4bf1e669d5
Kevin D <42454194+Kjay1181@users.noreply.github.com>
2018-10-15 14:00:03 -1000

    Add files via upload

---
+++ hotel.js
@@ -0,0 +1,8 @@
+function addName(){
+  var x =document.getElementById("userInput").value;
+document.getElementById("userInput").value="";
+var name=document.createElement(name);
+var room= document.createTextNode(room)
+name.appendChild(room)
+  
+}
