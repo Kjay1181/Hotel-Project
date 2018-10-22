68dd821cbd922609625b6d00be0d6357576a1b6e
Kevin D <42454194+Kjay1181@users.noreply.github.com>
2018-10-22 13:59:38 -1000

    Update hotel.js

--- hotel.js
+++ hotel.js
@@ -1,12 +1,15 @@
+
 function addName(){
+  
   var x =document.getElementById("userInput").value;// gets the value of the user
 document.getElementById("userInput").value=""; // clears the text box name
 var li = document.createElement('li');
     var newText=document.createTextNode(x);
     li.appendChild(newText);
-   var olTag = document.getElementsByTagName('ol')[0];
+   var olTag = document.getElementById("kj");
     olTag.appendChild(li);
-    
+
+  
 }
 function removeName(){
   var remove=document.getElementById("userInput").value;
@@ -14,4 +17,3 @@ function removeName(){
   container.removeChild(elim);
 }
 
- 
